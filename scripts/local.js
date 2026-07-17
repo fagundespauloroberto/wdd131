63462b7f9511372dbb9cd69d180a8243c72e8470
fagundespauloroberto <paulofagundes.infohome@gmail.com>
2026-07-16 23:39:22 -0300

    dasdsa

--- scripts/local.js
+++ scripts/local.js
@@ -1,15 +1,21 @@
-function computeWindChillF(tempF, speed) {
-    return 35.74 + (0.6215 * tempF) - (35.75 * Math.pow(speed, 0.16)) + (0.4275 * tempF * Math.pow(speed, 0.16));
-}
-
-function computeWindChillC(tempC, speed) {
+function calcularSensacaoTermica(tempC, speed) {
     return 13.12 + (0.6215 * tempC) - (11.37 * Math.pow(speed, 0.16)) + (0.3965 * tempC * Math.pow(speed, 0.16));
 }
 
-let tempC = 10;
-let speed = 5;
-if (tempC <= 10 && speed > 4.8) {
-  document.getElementById("wcf").innerHTML = `${computeWindChillC(tempC, speed).toFixed(1)} °C`;
-} else {
-    document.getElementById("wcf").innerHTML = "N/D";
-}
+document.addEventListener("DOMContentLoaded", () => {
+    
+    document.getElementById("ano-atual").textContent = new Date().getFullYear();
+    document.getElementById("ultima-modificacao").textContent = document.lastModified;
+
+    const tempC = 10; //temperatura estática do local (Celsius)
+    const speed = 5;  // velocidade do vento estática (km/h)
+
+    const wcfElement = document.getElementById("wcf");
+
+    if (tempC <= 10 && speed > 4.8) {
+        const sensacao = calcularSensacaoTermica(tempC, speed);
+        wcfElement.textContent = `${sensacao.toFixed(1)} °C`;
+    } else {
+        wcfElement.textContent = "N/A";
+    }
+});
