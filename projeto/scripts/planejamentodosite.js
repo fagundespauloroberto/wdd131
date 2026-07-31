0f9ee38c7a4ae928095b0d2b28bfa25fb720288c
fagundespauloroberto <paulofagundes.infohome@gmail.com>
2026-07-30 21:00:30 -0300

    sadasds

--- projeto/scripts/planejamentodosite.js
+++ projeto/scripts/planejamentodosite.js
@@ -1,4 +1,3 @@
-// exibição das datas no rodapé
-let d = new Date();
-document.getElementById("ano-atual").innerHTML = `&copy;${d.getFullYear()}`;
-document.querySelector('#lastModified').textContent = `Última Modificação: ${document.lastModified}`;
+
+document.getElementById("ano-atual").textContent = new Date().getFullYear();
+document.getElementById("ultima-modificacao").textContent = document.lastModified;
